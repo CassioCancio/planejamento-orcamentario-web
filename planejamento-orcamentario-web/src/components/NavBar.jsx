24b4c149c57512e28a3d123810c48f9b4af7e4cc
CassioCancio <cassio.acancio@gmail.com>
2023-09-17 01:52:50 -0300

    Create nav bar and expense creation page

---
+++ planejamento-orcamentario-web/src/components/NavBar.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './NavBar.css';
+import LogoReitoria from '../images/logo-prp-usp.png'
+import LogoUsuario from '../images/logo-usuario-minimalista.png'
+
+function NavBar() {
+    return (
+        <div className='navigationBar'>
+            <div className='leftNavigationBar'>
+            <a href="/" className='logoNavBar'><img src={LogoReitoria} alt='logo da pró reitoria de pesquisa'/></a>
+            <a href="/" className='buttonNavBar'> Home       </a>
+            <a href="/" className='buttonNavBar'> Despesas   </a>
+            <a href="/" className='buttonNavBar'> Receitas   </a>
+            <a href="/" className='buttonNavBar'> Relatórios </a>
+            </div>
+
+            <div className='rightNavigationBar'>
+            <a href="/" className='userNavButton'><img src={LogoUsuario} alt='logo da pró reitoria de pesquisa'/>Cássio Cancio</a>
+            </div>
+        </div>
+    );
+}
+
+export default NavBar;
---
+++ planejamento-orcamentario-web/src/pages/ExpenseCreate.jsx
@@ -0,0 +1,59 @@
+import NavBar from "../components/NavBar"
+import './ExpenseCreate.css';
+
+function Expense() {
+
+  return (
+    <div>
+        <NavBar />
+
+        <h1>Insira os dados da nova despesa</h1>
+
+
+        <form className="createExpenseForm">
+          <div className="labelInput">
+            <label>Título</label><input placeholder="Edital para ..."></input>
+          </div>
+
+          <div className="display">
+            <div className="labelInput">
+              <label>Grupo da despesa</label>
+              <select id="selectGroup">
+                <option value="">Selecione uma opção</option>
+              </select>
+
+            </div>
+            <div className="labelInput">
+              <label>Categoria de despesa</label>
+              <select>
+                <option value="">Selecione uma opção</option>
+              </select>
+            </div>
+          </div>
+          
+          <div className="labelInput">
+            <label>Balanço</label><input placeholder="Edital para ..."></input>
+          </div>
+          <div className="labelInput">
+            <label>Descrição</label><input placeholder="Edital para ..."></input>
+          </div>
+          <div className="labelInput">
+            <label>Data de criação</label><input placeholder="Edital para ..."></input>
+          </div>
+          <div className="labelInput">
+            <label>Data de pagamento</label><input placeholder="Edital para ..."></input>
+          </div>
+          <div className="labelInput">
+            <label>Valor inicial</label>
+            <input type="number" step="0.01" name="quantity" min="0.01"></input>
+          </div>
+          <button></button>
+        </form>
+    </div>
+  );
+
+
+
+}
+
+export default Expense;
