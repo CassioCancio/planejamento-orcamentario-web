f366fc4891ac1724a07d3797804e1f1cf5c09447
larissavmss <larissavitoriamedeiros@usp.br>
2023-10-10 17:34:06 -0300

    change backend urls

--- planejamento-orcamentario-web/src/services/groupService.js
+++ planejamento-orcamentario-web/src/services/groupService.js
@@ -1,7 +1,7 @@
 export const getAllGroups = async () => {
     let groups = [];
     try {
-        const response = await fetch('http://localhost:8080/group'); // TODO: variavel global
+        const response = await fetch('http://localhost:8080/group/getAll'); // TODO: variavel global
         groups = await response.json();
     } catch (error) {
         console.error('Erro:'+ error);
